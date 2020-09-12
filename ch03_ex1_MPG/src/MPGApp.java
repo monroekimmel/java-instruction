077f936dacb68a4c4983edb7b30871776fbd7986
monroekimmel <monroekimmel@gmail.com>
2020-09-12 13:54:43 -0400

    New practice

---
+++ ch03_ex1_MPG/src/MPGApp.java
@@ -0,0 +1,36 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class MPGApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Miles Per Gallon calculator");
+        System.out.println();  // print a blank line
+
+        @SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+        
+        String choice = "y";
+        while (choice.equalsIgnoreCase("y")) {
+            System.out.print("Enter miles driven: ");
+            double miles = sc.nextDouble();
+            
+            System.out.print("Enter gallons of gas used: ");
+            double gallons = sc.nextDouble();
+            
+            double mpg = miles/gallons;
+            NumberFormat mpgFormatter = NumberFormat.getNumberInstance();
+            mpgFormatter.setMaximumFractionDigits(3);
+            
+            String mpgFormatted = mpgFormatter.format(mpg);
+            
+            System.out.println("Miles per gallon is " + mpgFormatted + ".");
+            System.out.println(); 
+            
+            System.out.print("Calculate another MPG? (y/n): ");
+            choice = sc.next();
+            System.out.println();
+        }
+    }
+    
+}
