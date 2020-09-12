42a011039d761464614bb0a4c000e8da1b864c05
monroekimmel <monroekimmel@gmail.com>
2020-09-12 13:56:26 -0400

    New practice

---
+++ ch03_TravelTimeCalculator/src/TravelTimeApp.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TravelTimeApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the travel time calculator!");
+		
+		Scanner sc =  new Scanner(System.in);
+		
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y"));
+			System.out.print("Enter miles: ");
+			double miles = sc.nextDouble();
+			
+			System.out.print("Enter miles per hour: ");
+			double mph = sc.nextDouble();
+			
+			int hours = (int)(miles / mph);
+			double milesRemaining = hours % mph;
+			double minutes = milesRemaining % MINUTES_PER_HOUR;
+	}
+
+}
