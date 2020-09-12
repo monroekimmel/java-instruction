50ae68b71e8a6fec08ec7eb92ed85d4977c7c1db
monroekimmel <monroekimmel@gmail.com>
2020-09-12 13:58:22 -0400

    Practice for da day

---
+++ ch04_ins_dowhile/src/DoWhileApp.java
@@ -0,0 +1,17 @@
+
+public class DoWhileApp {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String word;
+		
+		do {
+			System.out.println("Say the magic word: ");
+			word = sc.next();
+		} while (word.equalsIgnoreCase("please"));
+		
+		System.out.println("Thank you! ");
+	}
+
+}
