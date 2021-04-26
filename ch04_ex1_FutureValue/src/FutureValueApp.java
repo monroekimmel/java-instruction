c0cb8b41396c7b4e6e00bd6e4e1b448dc83ac283
monroekimmel <monroekimmel@gmail.com>
2021-04-26 19:23:40 -0400

    Practice

--- ch04_ex1_FutureValue/src/FutureValueApp.java
+++ ch04_ex1_FutureValue/src/FutureValueApp.java
@@ -10,7 +10,6 @@ public class FutureValueApp {
         Scanner sc = new Scanner(System.in);
         String choice = "y";
         while (choice.equalsIgnoreCase("y")) {
-            // get the input from the user
             System.out.print("Enter monthly investment:   ");
             double monthlyInvestment = sc.nextDouble();
             System.out.print("Enter yearly interest rate: ");
@@ -18,24 +17,20 @@ public class FutureValueApp {
             System.out.print("Enter number of years:      ");
             int years = sc.nextInt();
 
-            // convert yearly values to monthly values
             double monthlyInterestRate = interestRate / 12 / 100;
             int months = years * 12;
 
-            // use a for loop to calculate the future value
             double futureValue = 0.0;
             for (int i = 1; i <= months; i++) {
                 futureValue = (futureValue + monthlyInvestment) * 
                               (1 + monthlyInterestRate);
             }
 
-            // format the result and display it to the user
             NumberFormat currency = NumberFormat.getCurrencyInstance();
             System.out.println("Future value:               "
                     + currency.format(futureValue));
             System.out.println();
 
-            // see if the user wants to continue
             System.out.print("Continue? (y/n): ");
             choice = sc.next();
             System.out.println();
