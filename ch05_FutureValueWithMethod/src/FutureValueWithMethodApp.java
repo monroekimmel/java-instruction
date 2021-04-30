dc320660b7ac1d420e7bc93d81ce8e74924439a1
monroekimmel <monroekimmel@gmail.com>
2021-04-30 16:18:05 -0400

    Practice

--- ch05_FutureValueWithMethod/src/FutureValueWithMethodApp.java
+++ ch05_FutureValueWithMethod/src/FutureValueWithMethodApp.java
@@ -8,7 +8,6 @@ public class FutureValueWithMethodApp {
 		Scanner sc = new Scanner(System.in);
         String choice = "y";
         while (!choice.equalsIgnoreCase("n")) {
-            // get the input from the user
             System.out.print("Enter monthly investment:   ");
             double monthlyInvestment = sc.nextDouble();
             System.out.print("Enter yearly interest rate: ");
@@ -16,21 +15,17 @@ public class FutureValueWithMethodApp {
             System.out.print("Enter number of years:      ");
             int years = sc.nextInt();
 
-            // convert yearly values to monthly values
             double monthlyInterestRate = interestRate / 12 / 100;
             int months = years * 12;
 
-            // call the future value method
             double futureValue = calculateFutureValue(monthlyInvestment,
                     monthlyInterestRate, months);
 
-            // format and display the result
             NumberFormat currency = NumberFormat.getCurrencyInstance();
             System.out.println("Future value:               "
                     + currency.format(futureValue));
             System.out.println();
 
-            // see if the user wants to continue
             System.out.print("Continue? (y/n): ");
             choice = sc.next();
             System.out.println();
