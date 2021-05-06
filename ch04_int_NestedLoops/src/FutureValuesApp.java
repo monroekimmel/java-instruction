503b7674eea770a2cc29dd00eb8db55d8ba6559a
monroekimmel <monroekimmel@gmail.com>
2021-05-06 18:21:29 -0400

    Practice

--- ch04_int_NestedLoops/src/FutureValuesApp.java
+++ ch04_int_NestedLoops/src/FutureValuesApp.java
@@ -4,15 +4,18 @@ public class FutureValuesApp {
 
 	public static void main(String[] args) {
 		// get the formatters
+		
 		NumberFormat currency = NumberFormat.getCurrencyInstance();
 		NumberFormat percent = NumberFormat.getCurrencyInstance();
 		percent.setMinimumFractionDigits(1);
 		
 		// set the display monthly payment
+		
 		int monthlyInvestment = 100;
 		System.out.println("Monthly investment: " + currency.format(monthlyInvestment) + "\n");
 		
 		// create header
+		
 		String table = **;
 		String headerRow + "Year      ";
 		
@@ -24,11 +27,14 @@ public class FutureValuesApp {
 		table += headerRow + "\n";
 		
 		//loop thru the years
+		
 		for (int year = 1; year < 7; year ++) {
 			//add year to the start of the row
+			
 			String row = year + "      ";
 			
 			//start nested loop
+			
 			for (double rate = 5.0; rate < 7.0; rate += 0.5) {
 				int month = year * 12;
 				double monthlyInterestRate = rate/12/100;
